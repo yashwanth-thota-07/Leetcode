f930f882dba8d101e28916db3b57c597c872e912
Yashwanth Thota <thota5366@gmail.com>
2026-08-28 21:31:48 +0530

    Time: 327 ms (33.95%) | Memory: 32.5 MB (51.78%) - LeetSync

---
+++ 4318-valid-subarrays-with-matching-sum-digits-i/valid-subarrays-with-matching-sum-digits-i.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int countValidSubarrays(vector<int>& nums, int x) {
+        int n = nums.size();
+        int count = 0;
+        long long sum = 0;
+        string s;
+        int x1, x2;
+        for(int i = 0;i<n;i++){
+            sum = 0;
+            for(int j = i;j<n;j++){
+                sum = sum + nums[j];
+                x1 = sum % 10;
+                long long temp = sum;
+                while (temp >= 10){
+                    temp /= 10;}
+                    x2 = temp;
+                    if(x1==x2 && x1==x){
+                        count++;
+                    }
+            }
+        }
+        return count;
+    }
+};
