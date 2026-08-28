e7a54e01178df795d34c058cc306d798cd83d543
Yashwanth Thota <thota5366@gmail.com>
2026-08-28 21:34:25 +0530

    Time: 347 ms (26.99%) | Memory: 32.3 MB (73.26%) - LeetSync

--- 4318-valid-subarrays-with-matching-sum-digits-i/valid-subarrays-with-matching-sum-digits-i.cpp
+++ 4318-valid-subarrays-with-matching-sum-digits-i/valid-subarrays-with-matching-sum-digits-i.cpp
@@ -6,18 +6,19 @@ public:
         long long sum = 0;
         string s;
         int x1, x2;
-        for(int i = 0;i<n;i++){
+        for (int i = 0; i < n; i++) {
             sum = 0;
-            for(int j = i;j<n;j++){
+            for (int j = i; j < n; j++) {
                 sum = sum + nums[j];
                 x1 = sum % 10;
                 long long temp = sum;
-                while (temp >= 10){
-                    temp /= 10;}
-                    x2 = temp;
-                    if(x1==x2 && x1==x){
-                        count++;
-                    }
+                while (temp >= 10) {
+                    temp = temp / 10;
+                }
+                x2 = temp;
+                if (x1 == x2 && x1 == x) {
+                    count++;
+                }
             }
         }
         return count;
